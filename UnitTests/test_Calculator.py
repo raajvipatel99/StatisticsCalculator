fe107442099860fac57ad8a267b6511530b738ac
raajvipatel99 <raajvipatel99@gmail.com>
2021-07-30 14:08:38 -0400

    Task: dont_Calculator renamed to test_Calculator

--- UnitTests/test_Calculator.py
+++ UnitTests/test_Calculator.py
@@ -12,33 +12,33 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculator)
 
     def test_addition(self):
-        test_data = CsvReader('../Unit Test Addition.csv').data
+        test_data = CsvReader('./Unit Test Addition.csv').data
         for row in test_data:
             self.assertAlmostEqual(self.calculator.add(row['Value 1'], row['Value 2']), float(row['Result']), 0)
 
     def test_subtraction(self):
-        test_data = CsvReader('../Unit Test Subtraction.csv').data
+        test_data = CsvReader('./Unit Test Subtraction.csv').data
         for row in test_data:
             self.assertAlmostEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), float(row['Result']), 0)
 
     def test_multiplication(self):
-        test_data = CsvReader('../Unit Test Multiplication.csv').data
+        test_data = CsvReader('./Unit Test Multiplication.csv').data
         for row in test_data:
             self.assertAlmostEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), float(row['Result']), 0)
 
     def test_division(self):
         # with self.assertRaises(ZeroDivisionError):
-            test_data = CsvReader('../Unit Test Division.csv').data
+            test_data = CsvReader('./Unit Test Division.csv').data
             for row in test_data:
                 self.assertAlmostEqual(self.calculator.divide(row['Value 1'], row['Value 2']), float(row['Result']), 0)
 
     def test_square(self):
-        test_data = CsvReader('../Unit Test Square.csv').data
+        test_data = CsvReader('./Unit Test Square.csv').data
         for row in test_data:
             self.assertAlmostEqual(self.calculator.square(row['Value 1']), float(row['Result']), 0)
 
     def test_squareroot(self):
-        test_data = CsvReader('../Unit Test Square Root.csv').data
+        test_data = CsvReader('./Unit Test Square Root.csv').data
         for row in test_data:
             self.assertAlmostEqual(self.calculator.squareroot(row['Value 1']), float(row['Result']), 0)
 
